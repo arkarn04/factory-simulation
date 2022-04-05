4984dc0d60575511ed2c3e00f9e65cc0c20b418a
arkarn04 <karnaryan253@gmail.com>
2022-04-06 01:54:20 +0530

    Intial Commit

---
+++ Testing/AdjusterTest.java
@@ -0,0 +1,45 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AdjusterTest {
+	@Test
+	public void testAdjuster() {
+		adjuster obj = new adjuster();
+		assertEquals("", obj.getId());
+		assertEquals(0,obj.getDaysWorked());
+	}
+
+	@Test
+	public void testGetID() {
+		adjuster obj = new adjuster();
+		obj.setId("Lathe");
+		assertEquals("Lathe", obj.getId());
+	}
+
+	@Test
+	public void testSetID() {
+		adjuster obj = new adjuster();
+		obj.setId("Lathe");
+		assertEquals("Lathe", obj.getId());
+	}
+
+	@Test
+	public void testAddDays() {
+		adjuster obj = new adjuster();
+		int days = obj.getDays(); 
+		obj.AddDays(100);
+		assertEquals(days+100, obj.getDays());
+	}
+
+	@Test
+	public void testGetDays() {
+		adjuster obj = new adjuster();
+		int days = obj.getDays(); 
+		obj.AddDays(100);
+		assertEquals(days+100, obj.getDays());
+	}
+
+}
